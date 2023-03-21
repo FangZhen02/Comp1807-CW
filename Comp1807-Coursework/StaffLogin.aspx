f6a0ebef9588b45ae9e55ff338c88ba276a42379
Amaleshwaren <Amaleshwaren06@gmail.com>
2023-03-21 21:25:53 +0800

    staff panel done

--- Comp1807-Coursework/StaffLogin.aspx
+++
@@ -1,3 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StaffLogin.aspx.cs" Inherits="Comp1807_Coursework.StaffLogin" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-</asp:Content>
--- Comp1807-Coursework/StaffPanel.aspx.cs
+++ Comp1807-Coursework/StaffPanel.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Comp1807_Coursework
 {
-    public partial class StaffLogin : System.Web.UI.Page
+    public partial class StaffPanel : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
