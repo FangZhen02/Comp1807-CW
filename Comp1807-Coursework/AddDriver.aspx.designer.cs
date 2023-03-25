3c239b2a1e18df5345333c6b42f5701900795e69
FangZhen <fangzhen0279@gmail.com>
2023-03-25 13:37:58 +0800

    fix missing driver name

--- Comp1807-Coursework/AddDriver.aspx.designer.cs
+++ Comp1807-Coursework/AddDriver.aspx.designer.cs
@@ -32,26 +32,9 @@ namespace Comp1807_Coursework
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtContact;
 
+        protected global::System.Web.UI.WebControls.TextBox txtDriverName;
         /// <summary>
         /// txtBrand control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
-
-        /// <summary>
-        /// lblExists control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblExists;
-
-        /// <summary>
-        /// RequiredFieldValidator2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
