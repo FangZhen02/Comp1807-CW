a710f46704862737a8370e444cf588b00ef7927e
FangZhen <fangzhen0279@gmail.com>
2023-04-03 14:25:54 +0800

    SignOut function

--- Comp1807-Coursework/Site.Master.designer.cs
+++ Comp1807-Coursework/Site.Master.designer.cs
@@ -15,6 +15,24 @@ namespace Comp1807_Coursework
     {
 
         /// <summary>
+        /// lblSiteUserName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSiteUserName;
+
+        /// <summary>
+        /// btnSignOut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSignOut;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
