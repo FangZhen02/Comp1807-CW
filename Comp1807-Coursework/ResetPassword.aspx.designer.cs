50e637e781814b8391f1403e35b36c3e22ee9835
FangZhen02 <125228769+FangZhen02@users.noreply.github.com>
2023-04-06 21:23:16 +0800

    new master (#41)
    Co-authored-by: Amaleshwaren <Amaleshwaren06@gmail.com>

--- Comp1807-Coursework/ResetPassword.aspx.designer.cs
+++ Comp1807-Coursework/ResetPassword.aspx.designer.cs
@@ -49,5 +49,14 @@ namespace Comp1807_Coursework
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnReset;
+
+        /// <summary>
+        /// btnreset2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnreset2;
     }
 }
