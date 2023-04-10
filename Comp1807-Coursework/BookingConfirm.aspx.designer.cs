80378ec64abf70841dc6a49deb46b3286f78ce4d
Amaleshwaren <Amaleshwaren06@gmail.com>
2023-04-10 09:46:35 +0800

    done editing

--- Comp1807-Coursework/BookingConfirm.aspx.designer.cs
+++ Comp1807-Coursework/BookingConfirm.aspx.designer.cs
@@ -60,15 +60,6 @@ namespace Comp1807_Coursework
         protected global::System.Web.UI.WebControls.Label Date;
 
         /// <summary>
-        /// Time control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Time;
-
-        /// <summary>
         /// minicab control.
         /// </summary>
         /// <remarks>
